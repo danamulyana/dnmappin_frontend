1d3eddad6ed8ddd1113680fddcf40dd42bd952ac
danamulyana <danabontot@gmail.com>
2021-07-06 21:12:40 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
   return (
     <div className="App">
-      hello
+      <ReactMapGL
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      />
     </div>
   );
 }
