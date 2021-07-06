787dda2201bd296aa8650ef86a1bcaa96b45fddc
danamulyana <danabontot@gmail.com>
2021-07-06 22:44:28 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import { Room, Star } from '@material-ui/icons'
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: "100vw",
+    height: "100vh",
+    latitude: -6.200000,
+    longitude: 106.816666,
+    zoom: 4
   });
   return (
     <div className="App">
       <ReactMapGL
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      />
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapStyle='mapbox://styles/danamulyana/ckqs74rau5aig17nw546f4c1o'
+      >
+      <Marker
+       latitude={-6.175392} 
+       longitude={106.827153} 
+       offsetLeft={-20} 
+       offsetTop={-10}
+      >
+        <Room style={{ fontSize: viewport.zoom * 7, color:"slateblue"}}/>
+      </Marker>
+      <Popup
+        latitude={-6.175392} 
+        longitude={106.827153}
+        closeButton={true}
+        closeOnClick={false}
+        anchor="left"
+      >
+        <div className="card">
+          <label>Place</label>
+          <h4 className="place">Monas</h4>
+          <label>Review</label>
+          <p>Beautiful place, i like it</p>
+          <label>Rating</label>
+          <div className="stars">
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+          <label>Information</label>
+          <span className="username">Created by <b>Dana Mulyana</b></span>
+          <span className="date">1 hour ago</span>
+        </div>
+      </Popup>
+      </ReactMapGL>
     </div>
   );
 }
